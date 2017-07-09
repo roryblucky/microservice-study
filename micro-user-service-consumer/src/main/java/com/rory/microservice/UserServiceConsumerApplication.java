67eeb392f913558ba99aaee44c739eb0fd03ae72
Rory Gao <roryblucky@gmail.com>
2017-07-09 21:51:33 +0800

    add eureka server and configuration
    Signed-off-by: Rory Gao <roryblucky@gmail.com>

--- micro-user-service-consumer/src/main/java/com/rory/microservice/UserServiceConsumerApplication.java
+++ micro-user-service-consumer/src/main/java/com/rory/microservice/UserServiceConsumerApplication.java
@@ -2,6 +2,7 @@ package com.rory.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
  * Created by RoryGao on 2017/7/9.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceConsumerApplication {
 
     @Bean
--- micro-user-service-provider/src/main/java/com/rory/microservice/UserServiceApplication.java
+++ micro-user-service-provider/src/main/java/com/rory/microservice/UserServiceApplication.java
@@ -2,11 +2,13 @@ package com.rory.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by RoryGao on 2017/7/9.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
